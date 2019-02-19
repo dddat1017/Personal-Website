80f74f0f011d4bc152bfbc27518bd398d0337c8e
Dat Do <datdo1017@gmail.com>
2019-02-19 09:51:56 -0800

    changed files and deleted folders

--- index.php
+++ index.php
@@ -72,22 +72,23 @@
             <div class="container">
                 <button class="accordion">Scraping Youtube Comments</button>
                 <div class="panel">
-                    <p>Project Insight: Scrape data (comments) from any Youtube video. Uses a browser simulator to work with the dynamics of Youtube web pages. Note, only the <em>comments</em> will be extracted, not any of the replies that may be under a comment. Tech stack: Python, Selenium, etc. <a href="https://github.com/dddat1017/Scraping-Youtube-Comments" target="_blank"><strong><em>Learn more!</em></strong></a></p>
-                    <img src="images/YTcomments_Scraping.png" alt="portfolio item" style="width:311px;height:200px;"/>
+                    <p>Project Insight: Scrape data (comments) from any Youtube video. Uses a browser automator to work with the dynamics of Youtube web pages. Note, only the <em>comments</em> will be extracted, not any of the replies that may be under a comment. Tech stack: Python, Selenium, etc. <a href="https://github.com/dddat1017/Scraping-Youtube-Comments" target="_blank"><strong><em>Learn more!</em></strong></a></p>
+					<img src="https://user-images.githubusercontent.com/40379856/53034446-05abeb80-3428-11e9-91c3-1025143846b0.gif" width="45%" style="max-width:100%;">
+					<img src="https://user-images.githubusercontent.com/40379856/53034445-05135500-3428-11e9-8e37-ed3830272d68.gif" width="45%" style="max-width:100%;">
                 </div>
                 <button class="accordion">Facial Detection and Recognition</button>
                 <div class="panel">
                     <p>Project Insight: Computer vision model that detects and recognizes real-life human faces. A neat implementation to this is that it collects and trains data almost instantaneously. Tech stack: Python, OpenCV, etc. <a href="https://github.com/dddat1017/Facial-Detection-Recognition" target="_blank"><strong><em>Learn more!</em></strong></a></p>
                     <img src="images/Face.gif" alt="portfolio item" style="width:311px;height:200px;"/>
                 </div>
-                <button class="accordion">Handwritten Digits Recognition</button>
+                <button class="accordion">Handwritten Digits Classification</button>
                 <div class="panel">
-                    <p>Project Insight: Convolutional neural network (CNN) model that recognizes handwritten digits from the MNIST Dataset. Tech stack: Python, Pytorch, Numpy, Matplotlib, etc. <a href="https://github.com/dddat1017/Handwritten-Number-Digits-Recognition" target="_blank"><strong><em>Learn more!</em></strong></a></li></h4></p>
+                    <p>Project Insight: Convolutional neural network (CNN) model that recognizes handwritten digits from the MNIST Dataset. Tech stack: Python, Pytorch, Numpy, Matplotlib, etc. <a href="https://github.com/dddat1017/Handwritten-Digits-Classification" target="_blank"><strong><em>Learn more!</em></strong></a></li></h4></p>
                     <img src="images/Numbers.gif" alt="portfolio item" style="width:311px;height:200px;"/>
                 </div>
-                <button class="accordion">Wizard Top Down Shooter Game</button>
+                <button class="accordion">Wizard Top Down Shooting Game</button>
                 <div class="panel">
-                    <p>Project Insight: Simple Wizard Top Down Shooter Game to become familiarized with writing code for a game. Interesting, easy way to get started. Tech stack: Java, Java Swing, Object Oriented Programming, etc. <a href="https://github.com/dddat1017/Wiz4rdGame" target="_blank"><strong><em>Learn more!</em></strong></a></p>
+                    <p>Project Insight: Simple Wizard Top Down Shooter Game to become familiarized with writing code for a game. Interesting, easy way to get started. Tech stack: Java, Java Swing, Object Oriented Programming, etc. <a href="https://github.com/dddat1017/Wizard-Top-Down-Shooting-Game" target="_blank"><strong><em>Learn more!</em></strong></a></p>
                     <img src="images/Wiz4rdGame.gif" alt="portfolio item" style="width:311px;height:200px;"/>
                 </div>
                 <button class="accordion">Research Paper</button>
@@ -105,7 +106,8 @@
                 <li>Full Stack Web Development (applying a structured stack to this website)</li>
                 <li>More Machine Learning/AI - Natural Language Processing | Reinforcement Learning</li>
                 <li>Deeper understanding of Data Structures and Algorithms</li>
-                <li>Gaming (getting familiar with Unity)</li>
+                <li>Game Development (getting familiar with Unity)</li>
+                <li>IOS & Android Software Development</li>
                 <li>Blockchain</li>
                 <li>And MORE to come</li>
             </ul>
