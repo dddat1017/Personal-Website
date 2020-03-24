c0ecc6ba0284e259db869bd36147517d82d46f05
Dat Do <datdo1017@gmail.com>
2020-03-24 03:17:30 -0700

    Pathfind: Refractored code.

--- scripts/bfs.js
+++ scripts/bfs.js
@@ -1,48 +1,10 @@
-// function to create matrix. Returns array consisted of [matrix, # of rows in matrix, # of cols in matrix].
-function generateMatrix(rows, cols) {
-    var matrix = [];
-    for (i = 0; i < rows; i++) {
-        matrix[i] = [];
-        for (j = 0; j < cols; j++) {
-            matrix[i][j] = i + "r" + "c" + j;
-        }
-    }
-    return [matrix, rows, cols];
-}
-
-// function to create matrix same as generateMatrix(), but this matrix is initialized with all boolean values 'false'.
-// used in order to keep track of which cell was visited.
-function generateVisitedMatrix(rr, cc) {
-    visited_matrix = generateMatrix(rr, cc)[0];
-    for(i = 0; i < rr; i++) {
-        for(j = 0; j < cc; j++) {
-            visited_matrix[i][j] = false;
-        }
-    }
-    return visited_matrix;
-}
-
-// function to create grid. Also, creates a matrix with same dimensions as the grid, using the generateMatrix() function. 
-// Returns array consisted of [grid, matrix].
-function generateGrid(r, c) {
-    var matrix = generateMatrix(r, c);
-    var grid = "<table>";
-    for (row = 0; row < r; row++) {
-        grid += "<tr>"; 
-        for (col = 0; col < c; col++) {      
-            grid += '<td id="' + row + 'r' + 'c' + col + '"' + '></td>';
-        }
-        grid += "</tr>"; 
-    }
-    return [grid, matrix];
-}
-
-// clicking and selecting cells.
+// Set-up. Wait for user to select the start/end/barrier.
 $(function() {
     var isMouseDown = false;
     var eyeD;
     var rc; 
     var counter = 0;
+    var htmlText = "";
 
     // on desktops/laptops.
     $('td').mousedown(function() {
@@ -56,11 +18,12 @@ $(function() {
             $(this).off('mousedown');
             $(this).off('mouseover');
             counter++;
-            m[rc[0]][rc[1]] = "S";
-            startR = rc[0];     // row value of where "S" is in the matrix.
-            startC = rc[1];     // col value of where "S" is in the matrix.
-            document.getElementById("instructions").innerHTML = "&#8618; Now choose the EXIT. (Click 'Clear' to reset)";
-            $('#okClear').append('<input id="reset" type="button" value="Clear" onClick="document.location.reload(true)">');
+            visited[rc[0]][rc[1]] = true;
+            sRC = new GridCell(rc[0], rc[1]);
+            htmlText += "&#8618; Now choose the EXIT. (Click 'Clear' to reset)";
+            document.getElementById("instructions").innerHTML = htmlText;
+            $('#okClear')
+                .append('<input id="reset" type="button" value="Clear" onClick="document.location.reload(true)">');
             $('#instructions').css('color', 'blue');
         } else if (counter < 2) {
             eyeD = $(this).attr('id');    // get 'id' of the 'td' clicked.
@@ -71,21 +34,24 @@ $(function() {
             $(this).off('mousedown');
             $(this).off('mouseover');
             counter++;
-            m[rc[0]][rc[1]] = "E";
-            endRC.push(rc[0]);
-            endRC.push(rc[1]);
-            document.getElementById("instructions").innerHTML = "&#8618; Now create your BARRIER/OBSTACLE. (Click 'Okay!' when you're done or 'Clear' to reset)";
-            $('#okClear').append('<input id="done" type="button" value="Okay!" style="background-color:#22272F;" onclick="run(); this.onclick=null;">');
+            eRC = new GridCell(rc[0], rc[1]);
+            htmlText = "&#8618; Now create your BARRIER/OBSTACLE. ";
+            htmlText += "(Click 'Okay!' when you're done or 'Clear' to reset)";
+            document.getElementById("instructions").innerHTML = htmlText;
+            htmlText = '<input id="done" type="button" value="Okay!" style="background-color:#22272F;" ';
+            htmlText += 'onclick="run(); this.onclick=null;">';
+            $('#okClear')
+                .append(htmlText);
             $('#instructions').css('color', 'red');
         } else {
-            eyeD = $(this).attr('id');    // get 'id' of the 'td' clicked.
+            eyeD = $(this).attr('id');  // get 'id' of the 'td' clicked.
 
             // extract the row and col from the 'id' of the 'td' clicked, and put them in an array.
             rc = eyeD.split('rc').map(Number);
             $(this).addClass("highlighted");
             $(this).off('mousedown');
             counter++;
-            m[rc[0]][rc[1]] = "#";
+            visited[rc[0]][rc[1]] = true;
             return false; // prevent text selection
         }
     });
@@ -100,7 +66,7 @@ $(function() {
             $(this).addClass("highlighted");
             $(this).off('mouseover');
             counter++;
-            m[rc[0]][rc[1]] = "#";
+            visited[rc[0]][rc[1]] = true;
         }
     }).bind("selectstart", function () {
         return false;
@@ -112,136 +78,194 @@ $(function() {
     });
 });
 
-// create.
-var table = generateGrid(20, 60);
-var g = table[0];   // grid with size R X C.
-var m = table[1][0];   // matrix from the array returned by generateMatrix(). 
-var R = table[1][1];   // number of rows from the array returned by generateMatrix().
-var C = table[1][2];   // number of columns from the array returned by generateMatrix().
-var startR;    // (initialize) row value of where "S" is, in the matrix.
-var startC;    // (initialize) col value of where "S" is, in the matrix.
-var endRC = [];    // empty matrix to store 'r, c' of "E" cell.
-
-$("#tableContainer").append(g);    // add the grid to html.
-
-var rq = [];    // row queue.
-var cq = [];    // col queue.
-
-// "visited matrix" of size RxC initialized with all boolean values 'false', to keep track of visited cells.
-// created with generateVisitedMatrix().
-var visited_m = generateVisitedMatrix(R, C);
-
-// direction vectors for going N,S,E,W,NE,NW,SE,SW.
-var up = [0, 1, -1];    
-var down = [0, 1, -1];
-
-// function to solve for the path. Returns a queue of all the "visited" cells.
-function solve() {
-    // enqueue the row value of the "S" cell to the row queue and col value to the col queue.
-    rq.push(startR);
-    cq.push(startC);
-
-    // "visited queue" to store each visited cell and its "parent". This will help retrace steps.
-    // form is [rVisited, cVisited, rParent, cParent].
-    var visited_q = [];
-    visited_q.push([startR, startC]);    // enqueue the [r, c] (no "parent") of the "S" cell to the "visited queue".
-
-    visited_m[startR][startC] = true;   // mark the "S" cell as visited in the "visited matrix".
-    while (rq.length > 0) {    // or (cq.size() > 0).
-        var rrr = rq.shift();   // dequeue the row queue.
-        var ccc = cq.shift();   // dequeue the col queue.
-        explore_neighbors(rrr, ccc, visited_q);    // pass in the dequeue values and the "visited queue"
-    }
-    return visited_q;   // return the visited queue.
-}
-
-// function to retrieve all possible "neighbors" of a cell. 
-// Parameters take row val of parent cell, col value of parent cell, and "visited queue".
-function explore_neighbors(rVal, cVal, q) {
-    // iteration of the loops correspond to the number of components in the direction vectors, which is 3.
-loop1:    
-    for (i = 0; i < 3; i++) {
-        var rNeighbor = rVal + up[i];    // row value of possible neighbor cell.
-loop2:
-        for (j = 0; j < 3; j++) {
-            var cNeighbor = cVal + down[j];    // col value of possible neighbor cell.
-
-            // if "out of bounds" (i.e. out of the matrix dimensions).
-            if (rNeighbor < 0 || cNeighbor < 0) { continue;} 
-            if (rNeighbor >= R || cNeighbor >= C) { continue;} 
-
-            // if already visited.
-            if (visited_m[rNeighbor][cNeighbor]) { continue;} 
-
-            // if a barrier cell.
-            if (m[rNeighbor][cNeighbor] === "#") { continue;}
-
-            // if the exit, "E", cell.
-            if (m[rNeighbor][cNeighbor] === "E") { 
-                // empty the row queue & col queue, BC we will want to break out of the while loop in solve().
-                rq = [];  
-                cq = [];
-
-                // enqueue [r"E", c"E", rParent, cParent] to the "visited queue".
-                q.push([rNeighbor, cNeighbor, rVal, cVal]);
-                visited_m[rNeighbor][cNeighbor] = true;    // mark this "E" cell as 'true' in "visited" matrix.
-                break loop1;    // break out of the nested loops.
+// Graph class representing the grid.
+class Graph {
+    constructor(r, c) {
+        this.numRows_ = r;
+        this.numCols_ = c;
+        this.grid_ = "<table>";
+        this.visited_ = [];
+        for (var row = 0; row < r; row++) {
+            this.grid_ += "<tr>"; 
+            this.visited_[row] = [];
+            for (var col = 0; col < c; col++) {      
+                this.grid_ += '<td id="' + row + 'r' + 'c' + col + '"' + '></td>';
+                this.visited_[row][col] = false;
             }
-
-            // enqueue the row value and col value of this newly found (and valid) neighbor cell into the 
-            // row queue and col queue, respectively.
-            rq.push(rNeighbor);
-            cq.push(cNeighbor);
-
-            // enqueue [rNeighbor, cNeighbor, rParent, cParent] to the "visited queue".
-            q.push([rNeighbor, cNeighbor, rVal, cVal]);
-            visited_m[rNeighbor][cNeighbor] = true;    // // mark this neighbor cell as 'true' in "visited" matrix.
+            this.grid_ += "</tr>"; 
         }
     }
-}
 
-// function to contruct the shortest path. Returns array of shortest path.
-function retrace(path_q) {
-    var path = [];
-    path.push([path_q[path_q.length - 1][0], path_q[path_q.length - 1][1]]);
-    var parentR = path_q[path_q.length - 1][2];
-    var parentC = path_q[path_q.length - 1][3];
+    getGrid() {
+        return this.grid_;
+    }
 
-    // start comparing from the second to last element.
-    for (i = (path_q.length - 2); i >= 0; i--) {   
-        if (parentR === path_q[i][0] && parentC === path_q[i][1]) {
-            path.unshift([path_q[i][0], path_q[i][1]]);
-            if (i === 0) {
-                parentR = 0;
-                parentC = 0;
+    getVisitedMatrix() {
+        return this.visited_;
+    }
+
+    getRows() {
+        return this.numRows_;
+    }
+
+    getCols() {
+        return this.numCols_;
+    }
+}  // Graph class.
+
+// GridCell class representing the row,col values of a cell in the grid.
+class GridCell {
+    constructor(r, c) {
+        this.row_ = r;
+        this.column_ = c;
+    }
+
+    getRow() {
+        return this.row_;
+    }
+
+    getColumn() {
+        return this.column_;
+    }
+
+    equals(other) {
+        return (this.row_ == other.getRow() && this.column_ == other.getColumn());
+    }
+
+    hashKey() {
+        return this.getRow() + 31 * this.getColumn();
+    }
+}  // GridCell class.
+
+// Array representation of a queue. First-in-first-out (FIFO).
+class Queue {
+    constructor() {
+        this.q_ = [];
+    }
+
+    add(item) {
+        this.q_.push(item);
+    }
+
+    remove() {
+        if (this.isEmpty())
+            return "Underflow";
+        return this.q_.shift();
+    }
+
+    isEmpty() {
+        return this.size() == 0;
+    }
+
+    size() {
+        return this.q_.length;
+    }
+}  // Queue class.
+
+// Search class which implements the BFS algorithm.
+class Search {
+    constructor(visitedMatrix, startRC, endRC, numRows, numCols) {
+        this.isSuccessful_ = false;
+
+        var queue_ = new Queue();
+        this.edgeTo_ = {};
+
+        queue_.add(startRC);
+        this.edgeTo_[startRC.hashKey()] = startRC;
+        visitedMatrix[startRC.getRow()][startRC.getColumn()] = true;
+
+        // direction vectors for going N,S,E,W,NE,NW,SE,SW.
+        var up = [0, 1, -1];
+        var down = [0, 1, -1];
+
+        var isTrue = true;
+        while ((!queue_.isEmpty()) && isTrue) {
+            var currentRC = queue_.remove();
+            var currentR = currentRC.getRow();
+            var currentC = currentRC.getColumn();
+            for (var i = 0; i < 3; i++) {
+                for (var j = 0; j < 3; j++) {
+                    var rNeighbor = currentR + up[i];
+                    var cNeighbor = currentC + down[j];
+                    if (!this.isSafe(visitedMatrix, rNeighbor, cNeighbor, numRows, numCols)) { continue; }
+                    if (this.isGoal(rNeighbor, cNeighbor, endRC)) {
+                        isTrue = false;
+                        this.isSuccessful_ = true;
+                    }
+                    
+                    visited[rNeighbor][cNeighbor] = true;
+                    var neighbor = new GridCell(rNeighbor, cNeighbor);
+                    this.edgeTo_[neighbor.hashKey()] = currentRC;
+                    queue_.add(neighbor);
+                }
             }
-            parentR = path_q[i][2]; 
-            parentC = path_q[i][3];
         }
     }
-    return path;
-}
 
-// when 'Okay!' is clicked. Where the magic all happens.
+    getPath() {
+        return this.edgeTo_;
+    }
+
+    isSuccessful() {
+        return this.isSuccessful_;
+    }
+
+    isSafe(visited, row, col, numRows, numCols) {
+        return (row >= 0) && (row < numRows) && (col >= 0) && (col < numCols) && (!visited[row][col]); 
+    }
+
+    isGoal(row, col, endRC) {
+        return (row == endRC.getRow()) && (col == endRC.getColumn());
+    }
+}  // Search class.
+
+// Create.
+var graph = new Graph(20, 60);
+var grid = graph.getGrid();
+var visited = graph.getVisitedMatrix();
+var rows = graph.getRows();
+var cols = graph.getCols();
+var sRC;
+var eRC;
+
+$("#tableContainer").append(grid);  // Add the grid to html.
+
+// When 'Okay!' is clicked. Where the magic all happens.
 function run() {
+    var bfs = new Search(visited, sRC, eRC, rows, cols);
     $('td').off();
-    var finish =  solve();
-    final_path = retrace(finish);
     document.getElementById("instructions").innerHTML = "&#8618; Please Wait until path is found!";
     $('#instructions').css('color', '#48D1CC');
 
-    var i = 1;
-    // call setInterval once. It will run itself repeatedly until the "E" is reached.
-    var t = setInterval(doSequence, 200);
+    var path =  bfs.getPath();
+    // Call setInterval once. It will run itself repeatedly until the goal is reached.
+    var step = path[eRC.hashKey()];
 
-    function doSequence() {
-        if ( i >= (final_path.length - 2)) {
-            document.getElementById("instructions").innerHTML = "&#8618; Click 'Clear' to reset.";
-            $('#instructions').css('color', 'black');
-            clearInterval(t);
+    var htmlText = "";
+    if (bfs.isSuccessful()) {
+        var actualPath = [];
+        while (!step.equals(sRC)) {
+            actualPath.unshift(step);
+            step = path[step.hashKey()];
         }
-        $("#" + final_path[i][0] + "r" + "c" + final_path[i][1]).css('background-color', '#48D1CC');
-        i++;
-    }
-}
 
+        var i = 0;
+        var t = setInterval(doSequence, 200);
+        function doSequence() {
+            if (i < actualPath.length) {
+                $("#" + actualPath[i].getRow() + "r" + "c" + actualPath[i].getColumn())
+                    .css('background-color', '#48D1CC');
+                i++;
+            } else {
+                htmlText += "&#8618; Path found! Click 'Clear' to reset.";
+                document.getElementById("instructions").innerHTML = htmlText;
+                $('#instructions').css('color', 'black');
+                clearInterval(t);
+            }
+        }
+    } else {
+        htmlText += "&#8618; No possible path found. Click 'Clear' to reset and try again!";
+        document.getElementById("instructions").innerHTML = htmlText;
+        $('#instructions').css('color', 'black');
+    } 
+}
